7a79d651617b216c38d78543f0a36b57280b268f
Yash Shirwant <G00423339@atu.ie>
2026-03-27 14:21:16 +0000

    Reservation API

---
+++ src/main/java/ie/atu/week8lab/Week8LabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week8lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week8LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week8LabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week8lab/Week8LabApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week8lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week8LabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
